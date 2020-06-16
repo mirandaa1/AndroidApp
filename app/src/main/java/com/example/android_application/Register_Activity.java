b16a3deab388100d6de738d8f9e09c414becb0d8
mirandaa1 <36954358+mirandaa1@users.noreply.github.com>
2020-06-16 18:45:38 +0200

    Login ang Register page.

---
+++ app/src/main/java/com/example/android_application/Register_Activity.java
@@ -0,0 +1,27 @@
+package com.example.android_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
+
+
+public class Register_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_);
+
+        TextView btn=findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register_Activity.this,login.class));
+            }
+        });
+    }
+}
