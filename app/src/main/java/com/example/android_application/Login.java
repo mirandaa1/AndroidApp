dca96b3c76b863541543fb805c235048d8f49a10
GresaPlakolli <GresaPlakolli@users.noreply.github.com>
2020-06-20 14:38:11 +0200

    Database connection
    Connect app with the database to save registration and
    Login information

--- app/src/main/java/com/example/android_application/Login.java
+++ app/src/main/java/com/example/android_application/Login.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    EditText e1,e2;
+    Button b1;
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,22 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(Login.this,Register_Activity.class));
             }
         });
+        db = new DatabaseHelper(this);
+        e1= (EditText)findViewById(R.id.inputUsername);
+        e2=(EditText)findViewById(R.id.inputPassword);
+        b1=(Button)findViewById(R.id.btnlogin);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = e1.getText().toString();
+                String password = e2.getText().toString();
+                Boolean Chkuserpass = db.usernamepassword(username,password);
+                if(Chkuserpass == true)
+                    Toast.makeText(getApplicationContext(),"Successfully Login",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(),"Wrong username or password",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/android_application/Register_Activity.java
+++ app/src/main/java/com/example/android_application/Register_Activity.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class Register_Activity extends AppCompatActivity {
+    DatabaseHelper db;
 
+    EditText e1,e2,e3,e4;
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_);
-
         TextView btn=findViewById(R.id.alreadyHaveAccount);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +27,40 @@ public class Register_Activity extends AppCompatActivity {
                 startActivity(new Intent(Register_Activity.this,Login.class));
             }
         });
+        db = new DatabaseHelper(this);
+        e4=(EditText)findViewById(R.id.inputUsername);
+        e1=(EditText)findViewById(R.id.inputName);
+        e2=(EditText)findViewById(R.id.inputPassword);
+        e3=(EditText)findViewById(R.id.inputConfirmPassword);
+        b1=(Button)findViewById(R.id.btnRegister);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1=e1.getText().toString();
+                String s2=e2.getText().toString();
+                String s3=e3.getText().toString();
+                String s4=e4.getText().toString();
+                if(s4.equals("") || s1.equals("") || s2.equals("") || s3.equals("")){
+                    Toast.makeText(getApplicationContext(),"Fields are empty",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if(s2.equals(s3)){
+                        Boolean chkemail = db.chkemail(s1);
+                        if(chkemail==true){
+                            Boolean insert = db.insert(s4,s1,s2);
+                            if(insert==true){
+                                Toast.makeText(getApplicationContext(),"Registered Successfully",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"Email already exists",Toast.LENGTH_SHORT).show();;
+                        }
+                    }
+                    Toast.makeText(getApplicationContext(),"Password do not match",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
