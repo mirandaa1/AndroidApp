f97504b3314c92c141dfa0be4ea7ed5e5e3ae9b2
mirandaa1 <36954358+mirandaa1@users.noreply.github.com>
2020-06-27 22:40:37 +0200

    Navgation Drawer
    Select which activity to be opened when a menu is selected
    Add toast when a menu is selected

--- app/src/main/java/com/example/android_application/Authors.java
+++ app/src/main/java/com/example/android_application/Authors.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Books extends AppCompatActivity {
+public class Authors extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_books);
-
+        setContentView(R.layout.activity_authors);
     }
 }
